a21c9cf45271701dafbc1d1998a09cc1ea35079c
xcalibur839 <NA>
2018-08-14 17:29:50 -0700

    Use default fit to screen initial-scale in viewport

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Gohan Ethermine Monitor</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width" />
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <link rel="icon" type="image/ico" href="favicon.ico" />
     <meta http-equiv="refresh" content="60" />
